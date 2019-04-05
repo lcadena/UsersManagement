d964f177cb7b256e603bf59f5c72f93fd1b3ed98
steph <stephypadilla08@gmail.com>
2019-04-05 14:39:17 +0200

    Eliminar user funciona

--- frontend/src/app/models/user.ts
+++ frontend/src/app/models/user.ts
@@ -1,11 +1,13 @@
 export class User {
+  _id: string;
   email: string;
   firstName: string;
   lastName: string;
   password: string;
   rol: string;
 
-  constructor(email = '', firstName = '', lastName = '', password = '', rol = ''){
+  constructor(_id = '', email = '', firstName = '', lastName = '', password = '', rol = ''){
+    this._id = _id;
     this.email = email;
     this.firstName = firstName;
     this.lastName = lastName;
--- frontend/src/app/services/userinfo.service.ts
+++ frontend/src/app/services/userinfo.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Product } from "../models/product";
 import { Environment } from "./environment";
 import {User} from "../models/user";
 import {Observable} from "rxjs";
