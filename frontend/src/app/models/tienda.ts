ccc893e0bdfeb3ff689db0553d99580ecc6e5408
CiliaSierra <cilia.ines.sierra@estudiant.upc.edu>
2019-04-24 20:15:47 +0200

    all models

--- frontend/src/app/models/tienda.ts
+++ frontend/src/app/models/tienda.ts
@@ -1,2 +1,15 @@
+import { Product } from './product';
+
 export class Tienda {
+    _id: string;
+    name: string;
+    direccion: string;
+    products: Product[];
+
+    constructor(_id='', name='', direccion='', products=[]){
+        this._id =_id;
+        this.name = name;
+        this.direccion = direccion;
+        this.products = products;
+    }
 }
