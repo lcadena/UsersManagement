555a625a6e83fb8110183c1a58009775d3d0d83a
steph <stephypadilla08@gmail.com>
2019-04-25 12:18:50 +0200

    cilia y sara ultimos cambios añadido tienda y ticket

---
+++ frontend/src/app/productsfromticket/productsfromticket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productsfromticket',
+  templateUrl: './productsfromticket.component.html',
+  styleUrls: ['./productsfromticket.component.css']
+})
+export class ProductsfromticketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
