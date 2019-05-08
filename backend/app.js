80ccbc6cc686cbd03304a002986c1e009cc937ee
CiliaSierra <cilia.ines.sierra@estudiant.upc.edu>
2019-05-08 19:31:28 +0200

    conexion de socket

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json())
 app.use(cors())
 //cabecera de cros mas restictiva
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin","http://localhost:8100");
+    res.header("Access-Control-Allow-Origin","https://localhost:8100");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
