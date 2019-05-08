b943db5de68044728775ac3b19c876f7beaa222c
CiliaSierra <cilia.ines.sierra@estudiant.upc.edu>
2019-05-08 19:13:55 +0200

    socket

--- backend/app.js
+++ backend/app.js
@@ -10,38 +10,16 @@ const app = express()
 const api_user = require('./routes/user')
 var swaggerUi = require('swagger-ui-express'),
     swaggerDocument = require('../swagger.json');
-
 //Método use
 app.use(bodyParser.urlencoded({ extended: false}))
-
 //Permitir peticiones con formato de mensaje JSON
 app.use(bodyParser.json())
-
-app.use(cors({origin: '*'}))
-
-//Ficheros hbs
-app.engine('.hbs', hbs({
-    defaultLayout: 'default',
-    extname: '.hbs'
-}))
-
-//View engine = .hbs
-app.set('view engine', '.hbs')
-
-
-//configurar el modo plantilla
-//le decimos a la app que en nuestra ruta /api utilize el modulo api
-app.use('/api', api_user)
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-
-//Renderizar login
-app.get('/login', (req, res) => {
-    res.render('login')
-})
-
+//Todos los permisos de CORS (acceso a todos los dominios)
+//app.use(cors({origin: '*'}))
+app.use(cors())
 //cabecera de cros mas restictiva
-app.use((req, res, next) =>{
-    res.header("Access-Control-Allow-Origin","http://localhost:4200");
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin","http://localhost:8100");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
@@ -51,5 +29,22 @@ app.use((req, res, next) =>{
     }
     next()
 })
+
+//Ficheros hbs
+app.engine('.hbs', hbs({
+    defaultLayout: 'default',
+    extname: '.hbs'
+}))
+//View engine = .hbs
+app.set('view engine', '.hbs')
+//configurar el modo plantilla
+app.use('/api', api_user)  //le decimos a la app que en nuestra ruta /api utilize el modulo api
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+//Renderizar login
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+
 
 module.exports = app
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,43 @@
 'use strict'
+var socket = require('socket.io')
+
 
 const mongoose = require('mongoose')
 const app = require('./app')
 const config = require('./config')
 
 mongoose.connect(config.db, (err, res) => {
-  if (err) {
-    return console.log(`Error al conectar a la base de datos: ${err}`)
-  }
+  if (err) {return console.log(`Error al conectar a la base de datos: ${err}`)}
   console.log('Conexión a la base de datos establecida...')
-  app.listen(config.port, () => {
+  app.listen(config.port, (err, res) => {
     console.log(`API REST corriendo en http://localhost:${config.port}`)
+    //server = res;
   })
+  //conexion de los socket
+  var io = socket(server);
+  io.on('connection', function(socket){
+    console.log("conexion del ususario con el SOCKET:  ", socket.id)
+    //recivo/envio a todos que me en conectado
+    socket.on('conectado', function(email){ //escucho que los socket que se conecten 
+      socket.nickname = email;
+      var allConnectedClients = io.sockets.connected;
+      Object.keys(allConnectedClients).forEach(function(key){
+        var val = allConnectedClients[key]["id"]+"+"+allConnectedClients[key]["email"];
+        send.push(val);
+      })
+      var send = []
+      io.sockets.emit('conectado', send); //se lo envio a todos los socket conectados
+      console.log("envio de la conexion ", send);  
+    })
+    //desconexion de los socket
+    socket.on('disconnect', function(){//escucho cuando se desconecta
+        var allConnectedClients = Object.keys(io.sockets.connected);
+      io.sockets.emit('conectado', allConnectedClients); //emito a todos los usuarios la nueva lista de conectados
+    })
+  })
+
+
+
+
+
 })
