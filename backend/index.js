53afb65e9847ece907f70e4d75d83504e07e6a04
CiliaSierra <cilia.ines.sierra@estudiant.upc.edu>
2019-05-09 08:15:08 +0200

    changes

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const config = require('./config')
 mongoose.connect(config.db, (err, res) => {
   if (err) {return console.log(`Error al conectar a la base de datos: ${err}`)}
   console.log('Conexión a la base de datos establecida...')
-  app.listen(config.port, (err, res) => {
+  server = app.listen(config.port, (err, res) => {
     console.log(`API REST corriendo en http://localhost:${config.port}`)
     //server = res;
   })
